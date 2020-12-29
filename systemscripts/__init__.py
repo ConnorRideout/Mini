80c47799a62921f035dbc8403df0861977f74388
CONNOR-PC\Cryden <connorrideout13@gmail.com>
2020-12-29 03:11:59 -0500

    v7.1

--- systemscripts/__init__.py
+++ systemscripts/__init__.py
@@ -1,11 +1,11 @@
-"""Scripts for use with command line"""
+"""Scripts for use from the command line"""
 
 
 import argparse
 from contextlib import redirect_stdout
 from io import StringIO
 from os import path as os_path, listdir as os_listdir
-from textwrap import *
+from textwrap import dedent
 from traceback import format_exc
 
 from ._msg import _show_info
@@ -14,6 +14,14 @@ from . import alterImages, createBorder, createSym, listItemsInDir, lnkToSym
 
 class _run:
     def __init__(self):
+        self.buildParser()
+        self.subs = {}
+        allFuncs = {na: fn for na, fn in globals().items() if na[0] != '_'}
+        for funcName in [f[:-3] for f in os_listdir(os_path.dirname(__file__)) if f[0] != '_' and f[-3:] == '.py']:
+            self.createHelp(funcName, allFuncs[funcName])
+        self.getArgs()
+
+    def buildParser(self):
         self.parser = argparse.ArgumentParser(
             prog=os_path.basename(os_path.dirname(__file__)),
             description=__doc__,
@@ -23,13 +31,8 @@ class _run:
             '-h', '--help', help="show this help message", action='store_true')
         self.parser.add_argument(
             '-w', '--window', help="redirect help and errors to a new window", action='store_true')
-        self.subs = {}
         self.subpars = self.parser.add_subparsers(
             help="sub-command help")
-        glos = {na: fn for na, fn in globals().items() if na[0] != '_'}
-        for funcName in [f[:-3] for f in os_listdir(os_path.dirname(__file__)) if f[0] != '_']:
-            self.createHelp(funcName, glos[funcName])
-        self.getArgs()
 
     def createHelp(self, name, script):
         subpar = self.subpars.add_parser(
@@ -62,10 +65,10 @@ class _run:
         except Exception:
             fname = "Main"
         if out:
-            _show_info(fname, out, all_args.window, 'help')
+            _show_info(all_args.window, "help", fname, out)
             raise SystemExit
         else:
             try:
                 all_args.func(*all_args.args)
             except Exception:
-                _show_info(fname, format_exc(), all_args.window)
+                _show_info(all_args.window, "error", fname, format_exc())
--- systemscripts/_msg.py
+++ systemscripts/_msg.py
@@ -1,8 +1,10 @@
 from os import system as os_sys
 
 
-def _show_info(script, output, win, msgType='error'):
+def _show_info(win, msgType, name, output):
     if win:
+        info = "{} {}:".format(name, msgType)
+        output = "\n".join(["="*25, info, "="*25, output])
         os_sys("""start "" powershell -command "write-host \\"{}\\""; pause""".format(
             output.replace('\n', '`n')))
     else:
