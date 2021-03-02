1e0cdf29f92a772722f942dc40eb311a6f0c28c1
CONNOR-PC\Cryden <connorrideout13@gmail.com>
2021-03-01 22:49:09 -0500

    v1.1

--- TrayIcon/__init__.py
+++ TrayIcon/__init__.py
@@ -1,101 +1,132 @@
-from threading import Thread, main_thread
-from pystray import *
-from time import sleep
+from threading import Thread as _Thread
+from PIL import Image as _Image, ImageDraw as _ImageDraw
+import pystray
 
 
-class TrayIconBuilder(Thread):
-	def __init__(self, title, icon, menu):
-		Thread.__init__(self, daemon=True)
-		if 'PIL.Image.Image' not in str(type(icon)):
-			from PIL import Image
-			try:
-				icon = Image.open(icon)
-			except Exception:
-				raise ValueError("icon argument must be a valid image path or a PIL.Image.Image instance")
-		if not isinstance(menu, (list, tuple)):
-			raise ValueError("menu argument must be an array of arrays. Any non-array value will become a separator.")
-		self.title, self.icon, self.menuItems = title, icon, []
-		self.menu = Menu(*self.__createMenu(menu))
+class TrayIcon(_Thread):
+    def __init__(self, title=None, icon=None):
+        _Thread.__init__(self, daemon=True)
+        if not icon:
+            self.icon = _Image.new('RGB', (32, 32), 'white')
+            icon_img_draw = _ImageDraw.Draw(self.icon)
+            icon_img_draw.rectangle((16, 0, 32, 16), fill='black')
+            icon_img_draw.rectangle((0, 16, 16, 32), fill='black')
+        elif 'PIL.Image.Image' not in str(type(icon)):
+            try:
+                self.icon = _Image.open(icon)
+            except Exception:
+                raise ValueError(
+                    "icon argument must be a valid image path or a PIL.Image.Image instance")
+        else:
+            self.icon = icon
+        self.title = title or "Python Tray Icon"
+        self.menu = None
 
-	def __createMenu(self, data, setvar=True):
-		menuOut,subOut = [],[]
-		for i in data:
-			subData = []
-			if isinstance(i, (list, tuple)):
-				txt,act,opt,*n = *i,{}
-				if isinstance(act, (list, tuple)):
-					act,subData = Menu(*(m:=self.__createMenu(act,False))[0]), m[1]
-				mItem = MenuItem(txt, act, **opt)
-				if setvar:
-					self.menuItems.append([mItem, subData])
-				else:
-					subOut.append([mItem, subData])
-			else:
-				mItem = Menu.SEPARATOR
-			menuOut.append(mItem)
-		return menuOut if setvar else [menuOut, subOut]
+    def run(self):
+        self.__tray_icon.run()
 
-	def getMenuItem(self, *pointer):
-		obj = self.menuItems[pointer.pop(0)]
-		try:
-			while pointer:
-				obj = obj[1][pointer.pop(0)]
-			return obj[0]
-		except IndexError:
-			return None
+    def show(self):
+        self.__tray_icon = pystray.Icon(name=self.title,
+                                        icon=self.icon,
+                                        title=self.title,
+                                        menu=self.menu)
+        self.start()
 
-	def run(self):
-		self.__tIcon = Icon(self.title, self.icon, self.title, self.menu)
-		self.__tIcon.run()
+    def hide(self):
+        self.__tray_icon.visible = False
+        self.__tray_icon.stop()
+        return
 
-	def show(self, show=True):
-		self.__tIcon.visible = show
+    def update(self):
+        self.__tray_icon.update_menu()
 
-	def stop(self):
-		self.show(False)
-		self.__tIcon.stop()
+    class Menu(pystray.Menu):
+        def __init__(self, parent=None):
+            self.__items = list()
+            pystray.Menu.__init__(self, lambda: self.__items)
+            if parent:
+                parent.menu = self
+            self.__radios = dict()
+            self.__checkboxes = dict()
 
+        def add_command(self, label, command, default=False, visible=True, enabled=True):
+            newItem = pystray.MenuItem(text=label,
+                                       action=command,
+                                       default=default,
+                                       visible=visible,
+                                       enabled=enabled)
+            self.__items.append(newItem)
 
-class example:
-	def __init__(self):
-		from PIL import Image, ImageDraw
-		from tkinter import Tk, Label
-		# create a window
-		self.root = Tk()
-		self.root.protocol("WM_DELETE_WINDOW", self.onClose)
-		Label(text='Hello').pack()
+        def add_radiobutton(self, label, command=None, group='A', checked=False, default=False, visible=True, enabled=True):
+            def set_state():
+                def inner(*_):
+                    self.__radios[group] = label
+                if command:
+                    command()
+                return inner()
 
-		# create trayicon image
-		# tIcon_img = Image.new('RGB', (32, 32), 'white')
-		# tIcon_img_draw = ImageDraw.Draw(tIcon_img)
-		# tIcon_img_draw.rectangle((16, 0, 32, 16), fill='black')
-		# tIcon_img_draw.rectangle((0, 16, 16, 32), fill='black')
-		tIcon_img = "C:\\Users\\Cryden\\AppData\\Roaming\\Spotify\\SpotifyPlayer_icon.png"
+            def get_state(*_):
+                def inner(*_):
+                    return self.__radios[group] == label
+                return inner()
 
-		# create trayicon menu
-		self.chk_state = False
-		tIcon_submenu_submenu = [("Happy", lambda:print("Yay!")),
-							  ("Sad", lambda:print("Aw..."))]
-		tIcon_submenu = [("Hi", lambda:print("Hello")),
-						  ("Bye", lambda:print("Goodbye")),
-						  ("Emotions", tIcon_submenu_submenu)]
-		tIcon_topmenu = [("Speak", tIcon_submenu),
-					  'Separator',
-					  ("Checkable", self.on_check, {'checked': lambda i: self.chk_state}),
-					  ("Minimize", self.root.iconify, {'default':True}),
-					  'Separator',
-					  ("Close", self.onClose)]
-	
-		# create trayicon and show it
-		self.tIcon = TrayIconBuilder('Test Icon', tIcon_img, tIcon_topmenu)
-		self.tIcon.start()
-		self.root.mainloop()
+            if checked:
+                self.__radios[group] = label
+            elif not self.__radios.get(group):
+                self.__radios[group] = ''
+            newItem = pystray.MenuItem(text=label,
+                                       action=set_state,
+                                       checked=get_state,
+                                       radio=True,
+                                       default=default,
+                                       visible=visible,
+                                       enabled=enabled)
+            self.__items.append(newItem)
 
-	def onClose(self):
-		self.tIcon.stop()
-		self.root.destroy()
-	
-	def on_check(self, icon, item):
-		self.chk_state = not item.checked
+        def add_checkbutton(self, label, command=None, checked=False, default=False, visible=True, enabled=True):
+            def on_check(_, item):
+                self.__checkboxes[label] = not item.checked
+                if command:
+                    command()
 
+            self.__checkboxes[label] = checked
+            newItem = pystray.MenuItem(text=label,
+                                       action=on_check,
+                                       checked=lambda _: self.__checkboxes[label],
+                                       default=default,
+                                       visible=visible,
+                                       enabled=enabled)
+            self.__items.append(newItem)
 
+        def add_cascade(self, label, menu, visible=True, enabled=True):
+            newItem = pystray.MenuItem(label,
+                                       menu,
+                                       visible=visible,
+                                       enabled=enabled)
+            self.__items.append(newItem)
+
+        def add_separator(self):
+            newItem = pystray.Menu.SEPARATOR
+            self.__items.append(newItem)
+
+        def insert(self, index, itemtype, **kwargs):
+            if itemtype == 'command':
+                self.add_command(**kwargs)
+            elif itemtype == 'radiobutton':
+                self.add_radiobutton(**kwargs)
+            elif itemtype == 'checkbutton':
+                self.add_checkbutton(**kwargs)
+            elif itemtype == 'cascade':
+                self.add_cascade(**kwargs)
+            elif itemtype == 'separator':
+                self.add_separator()
+            else:
+                raise ValueError("unsupported value for itemtype")
+            v = self.__items.pop()
+            self.__items.insert(index, v)
+
+        def get_radiobutton(self, group='A'):
+            return self.__radios[group]
+
+        def get_checkbutton(self, label):
+            return self.__checkboxes[label]
