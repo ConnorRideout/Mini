4dd601bb6b813d8f104bbce57d93262d0f886de1
CONNOR-PC\Cryden <connorrideout13@gmail.com>
2021-03-01 22:36:18 -0500

    v2.0

--- joinwith.py
+++ joinwith.py
@@ -3,11 +3,12 @@ if _TYPE_CHECKING:
     from typing import Iterable, Optional as O
 
 
-def joinwith(__iterable: "Iterable", __separator: str, __lastseparator: str, __before: "O[str]" = None, __after: "O[str]" = None):
-    b = __before if __before else ''
-    a = __after if __after else ''
+def joinwith(__iterable: "Iterable", __separator: str, __lastseparator: str, __formatstr: "O[str]" = None):
     joinlist = list()
+    doformat = True if set('{}') <= set(str(__formatstr)) else False
     for i in __iterable:
-        joinlist.append(f'{b}{i}{a}')
+        if doformat:
+            i = __formatstr.format(i)
+        joinlist.append(str(i))
     last = joinlist.pop()
     return f'{__separator.join(joinlist)}{__lastseparator}{last}'
