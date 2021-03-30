4393555a587286919ac71e8ee08c86fcabf32180
CONNOR-PC\Cryden <connorrideout13@gmail.com>
2021-03-30 15:15:24 -0400

    v2.1

--- joinwith.py
+++ joinwith.py
@@ -1,14 +1,15 @@
-from typing import TYPE_CHECKING as _TYPE_CHECKING
-if _TYPE_CHECKING:
-    from typing import Iterable, Optional as O
+from typing import Iterable, Optional as O
 
 
-def joinwith(__iterable: "Iterable", __separator: str, __lastseparator: str, __formatstr: "O[str]" = None):
+def joinwith(__iterable: Iterable, __separator: str, __lastseparator: str, __formatstr: O[str] = None):
     joinlist = list()
     doformat = True if set('{}') <= set(str(__formatstr)) else False
     for i in __iterable:
         if doformat:
             i = __formatstr.format(i)
         joinlist.append(str(i))
-    last = joinlist.pop()
-    return f'{__separator.join(joinlist)}{__lastseparator}{last}'
+    if len(joinlist) > 1:
+        last = joinlist.pop()
+        return f'{__separator.join(joinlist)}{__lastseparator}{last}'
+    else:
+        return joinlist[0]
