fca2b8b950e5d5d76924f438cbfb227e88485d41
CONNOR-PC\Cryden <connorrideout13@gmail.com>
2021-05-22 05:41:10 -0400

    v2.2

--- joinwith.py
+++ joinwith.py
@@ -1,7 +1,26 @@
-from typing import Iterable, Optional as O
+from typing import Iterable, Optional
 
 
-def joinwith(__iterable: Iterable, __separator: str, __lastseparator: str, __formatstr: O[str] = None):
+def joinwith(__iterable: Iterable, __separator: str, __lastseparator: str, __formatstr: str = None) -> str:
+    """-----
+    Join the elements of an iterable into a string, with formatting and a special last separator.
+
+    Parameters
+    ----------
+    __iterable (Iterable): The iterable to join
+
+    __separator (str): The string to put between all but the last two iterable elements
+
+    __lastseparator (str): The string to put between the last two iterable elements
+
+    __formatstr (str, optional): [default=None] A format string to apply to all elements before joining
+
+
+    Returns:
+    --------
+    str : The joined, formatted string
+    """
+
     joinlist = list()
     doformat = True if set('{}') <= set(str(__formatstr)) else False
     for i in __iterable:
